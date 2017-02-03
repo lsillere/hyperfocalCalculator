ef575fabe84e2ed1a77880937590a33d442b1a49
Loïc Sillere <loic.sillere@gmail.com>
2017-02-03 10:46:40 +0100

    Initial Commit

---
+++ Hyperfocal calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hyperfocal calculator
+//
+//  Created by Loic Sillere on 03/02/2017.
+//  Copyright © 2017 Loic Sillere. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
